dbea0c2aed652cd84ebb13620f3102b6586dd8f7
Wiktor Musial <wiktormusial@icloud.com>
2022-02-03 22:18:26 +0100

    Create City model

--- houses/models.py
+++ houses/models.py
@@ -3,10 +3,20 @@ from django.db import models
 from utils.models import TimeStampedModel
 
 
+class City(TimeStampedModel):
+    title = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
+
+
 class House(TimeStampedModel):
     title = models.CharField(max_length=150)
     desc = models.TextField()
     address = models.CharField(max_length=200)
+    city = models.ForeignKey(City,
+                             related_name="city",
+                             on_delete=models.CASCADE)
     qm = models.IntegerField(validators=[MinValueValidator(1),
                                          MaxValueValidator(300)])
 
--- houses/tests/tests_models.py
+++ houses/tests/tests_models.py
@@ -1,11 +1,18 @@
 from django.test import TestCase
-from houses.models import House
+from houses.models import House, City
 
 
 class HouseTestCase(TestCase):
-    def test_houses_return_title(self):
+    def setUp(self):
+        City.objects.create(title="Koeln")
+        c = City.objects.get(title="Koeln")
         House.objects.create(title="test_house", desc="good house near see",
-                             address="House 123, 51109 Kolln", qm=62)
-        house = House.objects.get(title="test_house")
+                             address="House 123, 51109 Kolln", city=c, qm=62)
 
-        self.assertEqual(house.__str__(), "test_house")
+    def test_houses_return_title(self):
+        h = House.objects.get(title="test_house")
+        self.assertEqual(h.__str__(), "test_house")
+
+    def test_house_return_city_name(self):
+        h = House.objects.get(title="test_house")
+        self.assertEqual(h.city.title, "Koeln")
