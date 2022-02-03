415b84d254f230369b3a62f7dc76eec8b2bc092e
Wiktor Musial <wiktormusial@icloud.com>
2022-02-04 00:44:10 +0100

    Create CitySerializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,11 @@
 from rest_framework import serializers
-from houses.models import House, PhotoHouse
+from houses.models import House, PhotoHouse, City
+
+
+class CitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = City
+        fields = ['id', 'title']
 
 
 class PhotoHouseSerializer(serializers.ModelSerializer):
@@ -10,6 +16,7 @@ class PhotoHouseSerializer(serializers.ModelSerializer):
 
 class DataSerializer(serializers.ModelSerializer):
     photos = PhotoHouseSerializer(many=True, read_only=True)
+    city = CitySerializer(read_only=True)
 
     class Meta:
         model = House
