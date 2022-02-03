8072f82c76568837df4fc14143aba02ebab2bff9
Wiktor Musial <wiktormusial@icloud.com>
2022-02-04 00:38:09 +0100

    Create PhotoHouse Serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,9 +1,14 @@
 from rest_framework import serializers
-from houses.models import House
+from houses.models import House, PhotoHouse
+
+
+class PhotoHouseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PhotoHouse
+        fields = ['id', 'title', 'house', 'main_photo', 'image']
 
 
 class DataSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = House
         fields = ['title', 'desc', 'address', 'qm', 'city', 'photos']
