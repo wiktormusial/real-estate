656ad679add3f4d4e46315b395d60f1712bcac01
Wiktor Musial <wiktormusial@icloud.com>
2022-02-04 17:49:13 +0100

    Fetch Data in Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,25 @@
-import React from "react"
+import React, { useEffect } from "react"
+import { useAppDispatch, useAppSelector } from "../../hooks/hooks"
+import { selectHousesStatus } from "../../store/houses/housesSlice"
+import { selectCitiesStatus } from "../../store/cities/citiesSlice"
+import { fetchHousesThunk } from "../../store/houses/housesThunks"
+import { fetchCitiesThunk } from "../../store/cities/citiesThunks"
 
 const Header: React.FC = () => {
+  const dispatch = useAppDispatch()
+  const citiesStatus = useAppSelector(selectCitiesStatus)
+  const housesStatus = useAppSelector(selectHousesStatus)
+
+  useEffect(() => {
+    if (citiesStatus === "idle") {
+      dispatch<void>(fetchCitiesThunk())
+    }
+
+    if (housesStatus === "idle") {
+      dispatch<void>(fetchHousesThunk())
+    }
+  }, [])
+
   return <div>Header</div>
 }
 
