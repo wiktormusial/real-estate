2b29a416f58bf4f75c68d80ad915532d531fed32
Wiktor Musial <wiktormusial@icloud.com>
2022-02-04 12:05:45 +0100

    Create CityViewSet

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
+from rest_framework import routers
 
-from .views import IndexView, GetData
+from .views import IndexView, GetData, CityViewSet
+
+router = routers.SimpleRouter()
+router.register(r'city', CityViewSet)
 
 urlpatterns = [
     path('', IndexView.as_view(), name="index"),
-    path('data/', GetData.as_view(), name="getdata")
+    path('data/', GetData.as_view(), name="getdata"),
 ]
+
+urlpatterns += router.urls
--- api/views.py
+++ api/views.py
@@ -1,8 +1,9 @@
+from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-from houses.models import House
-from .serializers import DataSerializer
+from houses.models import House, City
+from .serializers import DataSerializer, CitySerializer
 
 
 class IndexView(APIView):
@@ -15,3 +16,8 @@ class GetData(APIView):
         queryset = House.objects.all()
         serializer = DataSerializer(queryset, many=True)
         return Response(serializer.data)
+
+
+class CityViewSet(viewsets.ModelViewSet):
+    serializer_class = CitySerializer
+    queryset = City.objects.all()
