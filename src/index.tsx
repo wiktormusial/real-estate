0c022060b046c49820c6b1c2968452fa73138c05
Wiktor Musial <wiktormusial@icloud.com>
2022-02-04 22:04:34 +0100

    Install Tailwind

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom"
 import Router from "./Router"
 import { store } from "./store/store"
 import { Provider } from "react-redux"
+import "./index.css"
 
 ReactDOM.render(
   <Provider store={store}>
