c549f528084d21bcc3051bda508ee93c2010b467
Wiktor Musial <wiktormusial@icloud.com>
2022-02-05 23:25:33 +0100

    Add Auth URLs

--- realestate/urls.py
+++ realestate/urls.py
@@ -6,5 +6,6 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('houses.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('api/v1/', include('api.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
