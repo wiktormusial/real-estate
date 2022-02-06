35324f3a45cf4ea7fbb0fe9471a4f1be9ffd56b0
Wiktor Musial <wiktormusial@icloud.com>
2022-02-06 13:18:43 +0100

    Cosmetic changes

--- src/components/Houses/HousesList.tsx
+++ src/components/Houses/HousesList.tsx
@@ -1,24 +1,37 @@
 import React from "react"
 import { useAppSelector } from "../../hooks/hooks"
 import { selectAllHouses } from "../../store/houses/housesSlice"
+import { selectSelectedCity } from "../../store/cities/citiesSlice"
 import HousesListElement from "./HousesListElement"
+import CitiesList from "../Cities/CitiesList"
 
 const HousesList: React.FC = () => {
+  const selectedCity = useAppSelector(selectSelectedCity)
   const houses = useAppSelector(selectAllHouses)
 
   let housesList: JSX.Element[] | string
 
   if (houses.length !== 0) {
-    housesList = houses.map((item) => {
-      return <HousesListElement key={item.id} item={item} />
-    })
+    if (selectedCity === "All") {
+      housesList = houses.map((item) => {
+        return <HousesListElement key={item.id} item={item} />
+      })
+    } else {
+      housesList = houses
+        .filter((item) => item.city.title === selectedCity)
+        .map((item) => {
+          return <HousesListElement key={item.id} item={item} />
+        })
+    }
   } else {
     housesList = "You have to add some houses"
   }
 
   return (
-    <div className="container -mt-48 grid grid-cols-1 place-items-center ">
-      <div className="h-3/4 w-8/12 overflow-y-auto overflow-x-hidden bg-white p-5 drop-shadow-2xl">
+    <div className="grid grid-cols-1 md:p-5">
+      <div className="bg-white p-5 drop-shadow-2xl">
+        <CitiesList />
+        <hr className="mb-5 mt-5" />
         {housesList}
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./src/**/*.{ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "appbar-blue": "#1A76D2",
+      },
+    },
   },
   plugins: [],
 }
